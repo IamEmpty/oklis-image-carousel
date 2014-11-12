d5e81afa3b90a95b9b6382f2dfa89c767be0c673
IamEmpty <dustonroad@gmail.com>
2014-11-13 00:33:12 +0300

    second gallery prepare for vertical view

--- js/base.js
+++ js/base.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
 	$('.b-image-gallery').oklisImageCarousel();
+    $('.b-image-gallery_vertical').oklisImageCarousel({
+        orientationIsVertical: true
+    });
 });
