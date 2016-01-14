c638731ede0457e0c08f7e08134acd38b4741a0e
IamEmpty <dustonroad@gmail.com>
2016-01-15 00:29:33 +0300

    from tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,95 +4,95 @@ module.exports = function( grunt ) {
   // Compile developer friendly environment
   // $ grunt serve
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				options: {
-					style: 'expanded'
-				},
-				files: {
-					'examples/css/oklis-image-carousel.css': 'css/oklis-image-carousel.scss'
-				}
-			}
-		},
-		jade: {
-			dist: {
-				options: {
-					pretty: true
-				},
-				files: {
-					'examples/index.html': 'jade/index.jade'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: 'css/*.scss',
-				tasks: [ 'sass' ]
-			},
-			html: {
-					files: 'jade/*.jade',
-					tasks: [ 'jade' ]
-			}
-			/*min: {
-			files: 'js/*.js',
-			tasks: ['min']
-			},*/
-			/*cssmin: {
-				files: 'css/*.css',
-				tasks: ['cssmin']
-			}*/
-		},
-		copy: {
-			main: {
-				expand: true,
-				cwd: 'js/',
-				src: '*.js',
-				dest: 'examples/js/',
-				flatten: true,
-				filter: 'isFile'
-			},
-			vendor: {
-				expand: true,
-				cwd: 'bower_components/jquery/dist/',
-				src: 'jquery.js',
-				dest: 'examples/js/',
-				flatten: true,
-				filter: 'isFile'
-			}
-		}
-		/*min: {
-		    'dist': {
-		        'src': ['js/oklis-image-carousel.js', 'js/base.js'],
-		        'dest': 'build/all-own.min.js'
-		    }
-		},
-		cssmin: {
-		    'dist': {
-		        'src': ['css/oklis-image-carousel.css'],
-		        'dest': 'build/oklis-image-carousel.min.css'
-		    }
-		}*/
-	});
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'examples/css/oklis-image-carousel.css': 'css/oklis-image-carousel.scss'
+        }
+      }
+    },
+    jade: {
+      dist: {
+        options: {
+          pretty: true
+        },
+        files: {
+          'examples/index.html': 'jade/index.jade'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'css/*.scss',
+        tasks: [ 'sass' ]
+      },
+      html: {
+          files: 'jade/*.jade',
+          tasks: [ 'jade' ]
+      }
+      /*min: {
+      files: 'js/*.js',
+      tasks: ['min']
+      },*/
+      /*cssmin: {
+        files: 'css/*.css',
+        tasks: ['cssmin']
+      }*/
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'js/',
+        src: '*.js',
+        dest: 'examples/js/',
+        flatten: true,
+        filter: 'isFile'
+      },
+      vendor: {
+        expand: true,
+        cwd: 'bower_components/jquery/dist/',
+        src: 'jquery.js',
+        dest: 'examples/js/',
+        flatten: true,
+        filter: 'isFile'
+      }
+    }
+    /*min: {
+        'dist': {
+            'src': ['js/oklis-image-carousel.js', 'js/base.js'],
+            'dest': 'build/all-own.min.js'
+        }
+    },
+    cssmin: {
+        'dist': {
+            'src': ['css/oklis-image-carousel.css'],
+            'dest': 'build/oklis-image-carousel.min.css'
+        }
+    }*/
+  });
 
 
-	grunt.loadNpmTasks('grunt-contrib-stylus');
-	grunt.loadNpmTasks('grunt-contrib-jade');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	//grunt.loadNpmTasks('grunt-yui-compressor');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-yui-compressor');
 
 
- 	// Compile production files
-	grunt.registerTask('build', [
-		'jade',
-		'sass',
-		'copy'
-	]);
+  // Compile production files
+  grunt.registerTask('build', [
+    'jade',
+    'sass',
+    'copy'
+  ]);
 
-	grunt.registerTask('default', [
-		'build',
-		'watch'
-	]);
+  grunt.registerTask('default', [
+    'build',
+    'watch'
+  ]);
 };
